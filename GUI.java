13fbc281efacbaa44451e5d61b725c1c0ce4f17b
wsxdrorange <rick.zhang@yahoo.com>
2016-07-12 22:45:38 -0500

    Update GUI.java
    GUI!

--- GUI.java
+++ GUI.java
@@ -1,4 +1,8 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -7,17 +11,19 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
-public class GUI extends JFrame{
+public class GUI extends JFrame implements ActionListener{
 	public GUI()
 	{
 		super("Minesweeper by Rick");
-		setSize(600, 600);
+		setSize(600,600);
 		setVisible(true);
 		setResizable(false);
 		createJMenuBar();
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		addButtons();
+		setLocationRelativeTo(null);
 	}
 	public void createJMenuBar()
 	{
@@ -44,13 +50,26 @@ public class GUI extends JFrame{
 		game.add(reset);
 		game.addSeparator();
 		game.add(exit);
+		exit.addActionListener(this);
 	}
 	public void addButtons()
 	{
-		JPanel panel1 = new JPanel(new BorderLayout());
+		JPanel panel1 = new JPanel();
 		ImageIcon smiley = new ImageIcon(this.getClass().getResource("Images/smileyFace.jpg"));
 		JButton smileyRestart = new JButton(smiley);
+		smileyRestart.setBounds(300,100,100,50);
+		panel1.setBounds(50, 50, 300, 300);
+		panel1.setLayout(null);
 		panel1.add(smileyRestart);
 		panel1.setVisible(true);
+		add(panel1);
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if (arg0.getActionCommand().equals("Exit"))
+		{
+			dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+		}
 	}
 }
