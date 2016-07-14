04bc49023ebe46f694962147f46205558672a9bf
wsxdrorange <rick.zhang@yahoo.com>
2016-07-14 11:47:17 -0500

    Update GUI.java
    Added some board functions

--- GUI.java
+++ GUI.java
@@ -17,6 +17,10 @@ import javax.swing.WindowConstants;
 
 public class GUI extends JFrame implements ActionListener{
 	private String difficulty;
+	private int BOARD_HEIGHT;
+	private int BOARD_WIDTH;
+	Cell playingBoard [][];
+	JButton Butons [][];
 	public GUI(String difficulty)
 	{
 		super("Minesweeper by Rick");
@@ -26,6 +30,10 @@ public class GUI extends JFrame implements ActionListener{
 		createJMenuBar();
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
+	}
+	public void createPlayingBoard(Cell[][] playingBoard)
+	{
+		this.playingBoard = playingBoard;
 	}
 	public void createJMenuBar()
 	{
@@ -69,20 +77,26 @@ public class GUI extends JFrame implements ActionListener{
 	}
 	public void addButons(int width, int height)
 	{
+		this.BOARD_WIDTH = width;
+		this.BOARD_HEIGHT = height;
+		
 		//Main Panel - holds all other panels
 		JPanel mainPanel = new JPanel();
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 		
 		//Restart Button - Panel 1
 		JPanel panel1 = new JPanel();
-		//ImageIcon smiley = new ImageIcon(this.getClass().getResource("Images/smileyFace.png"));
-		JButton smileyRestart = new JButton("Restart");
+		ImageIcon smiley = new ImageIcon(this.getClass().getResource("Images/smileyFaceSmaller.png"));
+		JButton smileyRestart = new JButton(smiley);
+		smileyRestart.addActionListener(this);
+		smileyRestart.setActionCommand("SmileyRestart");
+		panel1.setLayout(new BoxLayout(panel1,BoxLayout.X_AXIS));
 		panel1.add(smileyRestart);
 		mainPanel.add(panel1);
 		
 		//Make Board - Panel 2
 		JPanel panel2 = new JPanel(new GridLayout(width,height));
-		JButton Butons[][] = new JButton[height][width];
+		Butons = new JButton[height][width];
 		for (int i = 0; i < Butons.length; i++)
 		{
 			for (int j = 0; j < Butons[0].length; j++)
@@ -97,9 +111,24 @@ public class GUI extends JFrame implements ActionListener{
 		//Add mainPanel to JFrame
 		add(mainPanel);
 	}
+	public JButton[][] getGUIBoard()
+	{
+		return this.Butons;
+	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
+		for (int i = 0; i < BOARD_HEIGHT; i++)
+		{
+			for (int j = 0; j < BOARD_WIDTH; j++)
+			{
+				if (arg0.getSource().equals(Butons[i][j]))
+				{
+					Butons[i][j].removeActionListener(this);
+					Butons[i][j].setText(playingBoard[i][j].toString());
+				}
+			}
+		}
 		if (arg0.getActionCommand().equals("Exit"))
 		{
 			dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
@@ -125,7 +154,7 @@ public class GUI extends JFrame implements ActionListener{
 			Board test = new Board("Hard");
 			setDefaultCloseOperation(3);
 		}
-		else if (arg0.getActionCommand().equals("Reset"))
+		else if (arg0.getActionCommand().equals("Reset") || arg0.getActionCommand().equals("SmileyRestart"))
 		{
 			setDefaultCloseOperation(HIDE_ON_CLOSE);
 			dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
