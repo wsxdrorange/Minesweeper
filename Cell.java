86b62001d4f56c664cae613936edf02b2a1e6441
wsxdrorange <rick.zhang@yahoo.com>
2016-07-15 12:05:04 -0500

    Update Cell.java
    Added a Revealed instance

--- Cell.java
+++ Cell.java
@@ -2,19 +2,30 @@
 public class Cell {
 	private int bombsNear;
 	private boolean isBomb;
+	private boolean revealed;
 	public Cell(boolean isBomb)
 	{
 		this.bombsNear = -1;
 		this.isBomb = true;
+		revealed = false;
 	}
 	public Cell(int bombsNear)
 	{
 		this.bombsNear = bombsNear;
 		this.isBomb = false;
+		revealed = false;
 	}
 	public boolean isBomb()
 	{
-		return isBomb;
+		return this.isBomb;
+	}
+	public void setRevealed(boolean revealed)
+	{
+		this.revealed = revealed;
+	}
+	public boolean isRevealed()
+	{
+		return this.revealed;
 	}
 	public String toString()
 	{
